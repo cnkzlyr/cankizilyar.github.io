99fe90d429eab230f4b351c6b652341c63b36a19
Can KIZILYAR <50930823+cnkzlyr@users.noreply.github.com>
2024-03-27 18:36:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const query = encodeURIComponent('(hukuk OR mahkeme OR boşanma)'); // Arama sorgusunu URL uyumlu hale getir
 
     // Haberleri çek
-    fetch(`https://api.newsdata.io/v1/news?q=${query}&language=tr&api_key=${apiKey}`)
+    fetch(`https://api.newsdata.io/v1/news?apikey=${apiKey}&q=${query}&language=tr`)
         .then(response => {
             if (!response.ok) {
                 throw new Error('Haberleri çekerken bir hata oluştu.');
@@ -14,13 +14,13 @@ document.addEventListener("DOMContentLoaded", function() {
             const newsList = document.getElementById('news-list');
             newsList.innerHTML = ''; // Önceki haberleri temizle
 
-            data.articles.forEach(article => {
+            data.data.forEach(article => {
                 const li = document.createElement('li');
                 const title = document.createElement('h3');
                 const description = document.createElement('p');
 
                 title.textContent = article.title;
-                description.textContent = article.description;
+                description.textContent = article.summary;
 
                 li.appendChild(title);
                 li.appendChild(description);
