27220bb84254a66c5b2facd068cbba2b575b0539
Can KIZILYAR <50930823+cnkzlyr@users.noreply.github.com>
2024-03-27 22:13:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
 document.addEventListener('DOMContentLoaded', async function () {
     const apiKey = 'pub_408328719e83be100b603329bbbbd86ee7566';
     const baseUrl = 'https://api.newsdata.io/v1';
-
+    const newsContainer = document.getElementById('news-container');
+    const logContainer = document.getElementById('log-container'); // Logları göstereceğimiz alan
+    
     try {
         const keywords = ["hukuk", "Fenerbahçe", "boşanma"];
         const response = await fetch(`${baseUrl}/news?apiKey=${apiKey}&q=${keywords.join(' AND ')}`);
         const data = await response.json();
-        const articles = data.articles;
-        const newsContainer = document.getElementById('news-container');
-
-        articles.forEach(article => {
-            const articleElement = document.createElement('div');
-            articleElement.classList.add('news-article');
-            articleElement.innerHTML = `
-                <h2>${article.title}</h2>
-                <p>${article.description}</p>
-                <a href="${article.url}" target="_blank">Haberi Oku</a>
-            `;
-            newsContainer.appendChild(articleElement);
-        });
+        logContainer.innerHTML = `<pre>${JSON.stringify(data, null, 2)}</pre>`; // API yanıtını HTML içine yaz
+        console.log(data); // API yanıtını konsola yazdır
+        
+        if(data.articles.length === 0) {
+            newsContainer.innerHTML = '<p>Haberler bulunamadı.</p>';
+        } else {
+            data.articles.forEach(article => {
+                const articleElement = document.createElement('div');
+                articleElement.classList.add('news-article');
+                articleElement.innerHTML = `
+                    <h2>${article.title}</h2>
+                    <p>${article.description}</p>
+                    <a href="${article.url}" target="_blank">Haberi Oku</a>
+                `;
+                newsContainer.appendChild(articleElement);
+            });
+        }
     } catch (error) {
         console.error('Haberler alınırken bir hata oluştu:', error);
-        const newsContainer = document.getElementById('news-container');
         newsContainer.innerHTML = '<p>Haberler alınamadı.</p>';
     }
 });
