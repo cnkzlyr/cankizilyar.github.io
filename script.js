747638c56f4d24f112fc7954cbd075d3c70398c1
Can KIZILYAR <50930823+cnkzlyr@users.noreply.github.com>
2024-03-27 22:10:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,27 @@
-const apiKey = 'pub_408328719e83be100b603329bbbbd86ee7566';
-const baseUrl = 'https://api.newsdata.io/v1';
+document.addEventListener('DOMContentLoaded', async function () {
+    const apiKey = 'pub_408328719e83be100b603329bbbbd86ee7566';
+    const baseUrl = 'https://api.newsdata.io/v1';
 
-async function fetchNews(keywords) {
     try {
+        const keywords = ["hukuk", "Fenerbahçe", "boşanma"];
         const response = await fetch(`${baseUrl}/news?apiKey=${apiKey}&q=${keywords.join(' AND ')}`);
         const data = await response.json();
-        return data.articles;
+        const articles = data.articles;
+        const newsContainer = document.getElementById('news-container');
+
+        articles.forEach(article => {
+            const articleElement = document.createElement('div');
+            articleElement.classList.add('news-article');
+            articleElement.innerHTML = `
+                <h2>${article.title}</h2>
+                <p>${article.description}</p>
+                <a href="${article.url}" target="_blank">Haberi Oku</a>
+            `;
+            newsContainer.appendChild(articleElement);
+        });
     } catch (error) {
         console.error('Haberler alınırken bir hata oluştu:', error);
-        return [];
+        const newsContainer = document.getElementById('news-container');
+        newsContainer.innerHTML = '<p>Haberler alınamadı.</p>';
     }
-}
-
-async function displayNews() {
-    const keywords = ["hukuk", "Fenerbahçe", "boşanma"];
-    const newsContainer = document.getElementById('news-container');
-    const articles = await fetchNews(keywords);
-
-    articles.forEach(article => {
-        const articleElement = document.createElement('div');
-        articleElement.classList.add('news-article');
-        articleElement.innerHTML = `
-            <h2>${article.title}</h2>
-            <p>${article.description}</p>
-            <a href="${article.url}" target="_blank">Haberi Oku</a>
-        `;
-        newsContainer.appendChild(articleElement);
-    });
-}
-
-document.addEventListener('DOMContentLoaded', displayNews);
+});
