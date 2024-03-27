0a0ba6fd1c0501365907ea485dbef2c90fbf78ce
Can KIZILYAR <50930823+cnkzlyr@users.noreply.github.com>
2024-03-27 18:22:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
     const apiKey = '183b6eb97e7e4ac4b5e6213afb8956ba'; // Haber API anahtarını buraya yerleştir
     const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-const apiUrl = `https://newsapi.org/v2/everything?q=(hukuk OR mahkeme OR boşanma)&language=tr&apiKey=${apiKey}`;
+    const apiUrl = `https://newsapi.org/v2/everything?q=(hukuk OR mahkeme OR boşanma)&language=tr&apiKey=${apiKey}`;
 
-fetch(proxyUrl + apiUrl)
-    .then(response => response.json())
-    .then(data => {
-        // Haberleri işle
-    })
-    .catch(error => {
-        // Hata durumunu işle
-    });
-
-    // Haberleri çek
-    fetch(`https://newsapi.org/v2/everything?q=(hukuk OR mahkeme OR boşanma)&language=tr&apiKey=${apiKey}`)
-        .then(response => response.json())
+    fetch(proxyUrl + apiUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Haberleri çekerken bir hata oluştu.');
+            }
+            return response.json();
+        })
         .then(data => {
             const newsList = document.getElementById('news-list');
             newsList.innerHTML = ''; // Önceki haberleri temizle
